abed43d6029d56a971b960e9218e8c757ee22530
profitproperly <illustriousmajesty@gmail.com>
2026-07-14 03:09:49 -0400

    Require server-side proceed confirmation in live verification

--- lensically-worker/src/index.ts
+++ lensically-worker/src/index.ts
@@ -12846,7 +12846,7 @@ async function handleOperatorMcpEngineeringTool(request: Request, env: Env, tool
         && allowed.status < 400
         && boundaryTest.blocked_before_proceed
         && boundaryTest.proceed_confirmed
-                && boundaryTest.continuation_ref_issued
+                        && boundaryTest.proceed_confirmation_recorded
         && boundaryTest.continuity_required_after_proceed
         && boundaryTest.continuity_resolved
         && boundaryTest.allowed_after_continuity,
