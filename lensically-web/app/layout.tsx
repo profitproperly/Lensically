2d3bb21d9e784bbe39f75adf1f2a3e0be6d72cbe
Brian <profit_properly_official@gmail.com>
2026-07-16 10:02:45 -0400

    Polish Lensically favicon tile

--- lensically-web/app/layout.tsx
+++ lensically-web/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
   icons: {
     icon: [
+      { url: "/lensically-icon-16.png", sizes: "16x16", type: "image/png" },
       { url: "/lensically-icon-32.png", sizes: "32x32", type: "image/png" },
       { url: "/lensically-icon-192.png", sizes: "192x192", type: "image/png" },
     ],
