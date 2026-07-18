94042d482e27a08332ced062c0979d5a4c99f2a8
profitproperly <illustriousmajesty@gmail.com>
2026-07-18 12:19:29 -0400

    Update client-safety incident receipt count

--- lensically-worker/test/systemDirectory.spec.ts
+++ lensically-worker/test/systemDirectory.spec.ts
@@ -467,7 +467,7 @@ describe("System Directory foundation", () => {
       intake_contract_version: "client-block-intake-v1",
       intake_mandatory: true,
       resume_allowed_only_after: "registry_validation_and_live_deployment",
-                                                prevented_client_block_count: 21,
+                                                prevented_client_block_count: 22,
       safe_request_profile_count: 13,
       universal_policy_count: 8,
       migrated_legacy_rule_count: 8,
