07f9e29ace153075f5c290128cd675ef621c2e62
profitproperly <illustriousmajesty@gmail.com>
2026-07-18 13:32:03 -0400

    Keep growth analytics readable during Guided Growth Mission discussion [operator-accounts]

--- lensically-worker/src/index.ts
+++ lensically-worker/src/index.ts
@@ -14902,7 +14902,7 @@ function operatorToolMutatesState(toolName: string): boolean {
     "listEngineeringAudit", "listOpsMemory", "readOpsMemory", "searchOpsMemory", "listPreCallRoutes", "selectOperatorKey", "confirmOperatorProceed", "getGrowthMission",
                         "planOperatorExecution", "getMcpAdminState", "getOperatorDecisionState", "getScheduledPostSchedulerState", "auditScheduledPost", "listMcpTools", "readMcpToolDefinition", "listImplementationBacklogItems", "getWorkflowStatus",
     "list_accounts", "get_account_state", "get_hourly_coverage", "get_manifest_review_batch", "get_production_board", "list_source_candidates", "get_source_candidate_batch",
-    "get_source_card", "list_active_gates", "list_strategy_memory", "list_scheduled_posts", "get_post_results",
+    "get_source_card", "list_active_gates", "list_strategy_memory", "list_scheduled_posts", "get_post_results", "get_monthly_growth_review", "get_performance_learning",
   ]);
   return !readOnly.has(toolName);
 }
