ec70791ec77969c65a4176b01de55d8060be5f05
profitproperly <illustriousmajesty@gmail.com>
2026-07-19 13:35:03 -0400

    Update client-safety incident count for compact segments

--- lensically-worker/test/systemDirectory.spec.ts
+++ lensically-worker/test/systemDirectory.spec.ts
@@ -863,7 +863,7 @@ describe("System Directory foundation", () => {
       intake_contract_version: "client-block-intake-v1",
       intake_mandatory: true,
       resume_allowed_only_after: "registry_validation_and_live_deployment",
-      prevented_client_block_count: 32,
+      prevented_client_block_count: 33,
       safe_request_profile_count: 19,
       universal_policy_count: 8,
       migrated_legacy_rule_count: 8,
