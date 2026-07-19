f06112c268851dfa77ab13a4674d4bfae5913818
profitproperly <illustriousmajesty@gmail.com>
2026-07-19 15:50:12 -0400

    Register second startup block

--- lensically-worker/test/systemDirectory.spec.ts
+++ lensically-worker/test/systemDirectory.spec.ts
@@ -886,7 +886,7 @@ describe("System Directory foundation", () => {
       intake_mandatory: true,
       resume_allowed_only_after: "registry_validation_and_live_deployment",
       prevented_client_block_count: 35,
-            safe_request_profile_count: 20,
+                  safe_request_profile_count: 21,
       universal_policy_count: 8,
       migrated_legacy_rule_count: 8,
     });
