ad7c9599426143b6b2ddd425a562dcc3287c9f93
profitproperly <illustriousmajesty@gmail.com>
2026-07-23 11:54:00 -0400

    Use the immutable cycle thesis for reused post influence

--- lensically-worker/src/index.ts
+++ lensically-worker/src/index.ts
@@ -12836,7 +12836,7 @@ async function persistManifestAutonomousPost(
         family_key: familyKey,
         generation_mode: generationMode,
         source_context: sourceContextValidation.value as unknown as Record<string, unknown>,
-        strategic_thesis: strategicThesis,
+                strategic_thesis: effectiveStrategicThesis,
         model_evaluation: { ...modelEvaluation, intelligence_application_assessment: intelligenceApplicationAssessment },
         semantic_repetition: {},
         experiment_assignment: reusedExperimentAssignment,
