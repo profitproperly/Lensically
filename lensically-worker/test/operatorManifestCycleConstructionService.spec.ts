6801766fead010ebf38631d9a120c37a2d91eee4
profitproperly <illustriousmajesty@gmail.com>
2026-07-31 19:05:45 -0400

    Align Main construction regression

--- lensically-worker/test/operatorManifestCycleConstructionService.spec.ts
+++ lensically-worker/test/operatorManifestCycleConstructionService.spec.ts
@@ -270,7 +270,7 @@ describe("Operator Manifest cycle construction service", () => {
             persistence_contract: {
         preferred_tool: "persist_manifest_autonomous_batch",
         fallback_tool: "persist_manifest_autonomous_post",
-        candidates_per_batch: { minimum: 1, maximum: 4 },
+                candidates_per_batch: { minimum: 1, maximum: 2 },
         generation_wave_size: 8,
         uninterrupted_until_terminal_or_blocked: true,
         regenerate_rejected_slots_only: true,
