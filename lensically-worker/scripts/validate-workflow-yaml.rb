7fb70b6ad02718dd6017d0c05fd6818ef44fe9aa
profitproperly <illustriousmajesty@gmail.com>
2026-08-15 05:50:10 -0400

    Annotate workflow YAML syntax failures

--- lensically-worker/scripts/validate-workflow-yaml.rb
+++ lensically-worker/scripts/validate-workflow-yaml.rb
@@ -5,6 +5,13 @@ files = Dir.glob(File.join(workflow_dir, "*.{yml,yaml}")).sort
 raise "no_workflow_files_found" if files.empty?
 
 files.each do |path|
-  YAML.parse_file(path)
-  puts "workflow_yaml_ok=#{File.basename(path)}"
+  begin
+    YAML.parse_file(path)
+    puts "workflow_yaml_ok=#{File.basename(path)}"
+  rescue Psych::SyntaxError => error
+    relative_path = path.delete_prefix("#{File.expand_path('../..', __dir__)}/")
+    problem = error.problem.to_s.gsub("%", "%25").gsub("\r", "%0D").gsub("\n", "%0A")
+    puts "::error file=#{relative_path},line=#{error.line},col=#{error.column}::#{problem}"
+    raise
+  end
 end
