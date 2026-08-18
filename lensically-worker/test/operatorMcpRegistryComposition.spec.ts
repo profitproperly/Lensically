9dcdb1cbc9a3705715533a6aac19833b34d8bef8
profitproperly <illustriousmajesty@gmail.com>
2026-08-18 00:19:55 -0400

    Regress effect-specific Step-4 lifecycle order

--- lensically-worker/test/operatorMcpRegistryComposition.spec.ts
+++ lensically-worker/test/operatorMcpRegistryComposition.spec.ts
@@ -76,7 +76,7 @@ describe("Operator MCP registry composition", () => {
       "operateStripe",
     ]));
 
-            const lifecycleOrder = ["getOperatorSessionMap", "getOperatorKnowledge", "getOperatorLiveState", "executeOperatorAction", "closeOperatorAction"];
+            const lifecycleOrder = ["getOperatorSessionMap", "getOperatorKnowledge", "getOperatorLiveState", "executeOperatorReadAction", "executeOperatorAction", "closeOperatorAction"];
     for (let index = 1; index < lifecycleOrder.length; index += 1) {
       expect(names.indexOf(lifecycleOrder[index - 1])).toBeLessThan(names.indexOf(lifecycleOrder[index]));
     }
